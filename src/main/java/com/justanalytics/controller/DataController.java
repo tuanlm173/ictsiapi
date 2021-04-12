7a00d697b75ac3ad6eeba3669980abead006b70a
tuanlm173 <tuanlm.cca@gmail.com>
2021-04-12 10:19:09 +0700

    remove jdbc spark databricks

--- src/main/java/com/justanalytics/controller/DataController.java
+++ src/main/java/com/justanalytics/controller/DataController.java
@@ -3,7 +3,6 @@ package com.justanalytics.controller;
 import com.justanalytics.entity.CarrierVisit;
 import com.justanalytics.response.RestEnvelope;
 import com.justanalytics.service.CarrierVisitService;
-import com.justanalytics.service.DataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,18 +11,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 public class DataController {
 
     private final CarrierVisitService carrierVisitService;
-    private final DataService dataService;
 
     @Autowired
-    public DataController(CarrierVisitService carrierVisitService, DataService dataService) {
+    public DataController(CarrierVisitService carrierVisitService) {
         this.carrierVisitService = carrierVisitService;
-        this.dataService = dataService;
     }
 
     @GetMapping(path = "/api/v1/getTopCarrierVisit", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -39,10 +35,10 @@ public class DataController {
         return ResponseEntity.ok(RestEnvelope.of(carrierVisitByTerminalAndDate));
     }
 
-    @GetMapping(path = "/api/v1/getQueryBasic", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<RestEnvelope> getArbitraryQueryForCarrierVisit(@RequestParam(value = "container") String container) {
-        String query = dataService.prepareQuery(container);
-        List<Map<String, Object>> results = dataService.getArbitraryQuery(query);
-        return ResponseEntity.ok(RestEnvelope.of(results));
-    }
+//    @GetMapping(path = "/api/v1/getQueryBasic", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<RestEnvelope> getArbitraryQueryForCarrierVisit(@RequestParam(value = "container") String container) {
+//        String query = dataService.prepareQuery(container);
+//        List<Map<String, Object>> results = dataService.getArbitraryQuery(query);
+//        return ResponseEntity.ok(RestEnvelope.of(results));
+//    }
 }
